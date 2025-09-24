fe16da479177b0e066d175b90fbac0ef8d4cd171
S1161110 <s1161110@gm.ncue.edu.tw>
2025-09-24 01:54:03 +0000

    circle area calculation

---
+++ temp.py
@@ -0,0 +1,7 @@
+import math
+def caculate_area(radius):
+    """Calculate the area of a circle given its radius."""
+    if radius < 0:
+        raise ValueError("Radius cannot be negative")
+    return math.pi * radius ** 2
+print(caculate_area(5))
